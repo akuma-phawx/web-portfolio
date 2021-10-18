7ee15d9cc95dbc18621ae409a1e54a0f233e8f5d
Christoforos Vradis <chrisvradis00@gmail.com>
2021-10-18 17:48:38 +0300

    Install eslint-config-google

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,12 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ["plugin:vue/vue3-essential", "eslint:recommended", "@vue/prettier"],
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/prettier",
+    "google",
+  ],
   parserOptions: {
     parser: "babel-eslint",
   },
