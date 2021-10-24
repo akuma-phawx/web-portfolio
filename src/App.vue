e5ea49c4ad7b3cad1075ca5809bed4aaf19aae8f
Christoforos Vradis <chrisvradis00@gmail.com>
2021-10-24 22:15:45 +0300

    Fix flex size

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container md:flex">
-    <Nav class="bg-blue-700" />
-    <router-view class="bg-yellow-600" />
+    <Nav class="bg-blue-700 h-1/6 w-full md:w-1/4 md:h-screen" />
+    <router-view class="bg-yellow-600 w-screen h-screen" />
   </div>
 </template>
 
