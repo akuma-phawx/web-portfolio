99e491d9c2defb11e7fa666db57f76d18fac0cfb
Christoforos Vradis <chrisvradis00@gmail.com>
2021-10-27 12:23:37 +0300

    Add svg icons - create footer component

--- src/components/nav_components/Nav.vue
+++ src/components/nav_components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container flex flex-col text-center">
     <div id="personal_image" class="flex flex-col justify-center pb-10">
-      <div class="image_container mx-auto pb-4">
+      <div class="image_container mx-auto pb-4 border-solid-2">
         <img
           src="../../assets/img/me.jpg"
           class="w-32 h-32 md:w-64 md:h-64"
@@ -13,14 +13,27 @@
       <h3>Fullstack Developer</h3>
     </div>
     <div id="nav " class="flex justify-center md:flex-col">
-      <router-link to="/"> Home </router-link>
-      <router-link to="/portfolio"> Portfolio </router-link>
-      <router-link to="/skills"> Skills </router-link>
-      <router-link to="/about"> About </router-link>
+      <router-link class="mr-4 md:mr-0" to="/"> Home </router-link>
+      <router-link class="mr-4 md:mr-0" to="/portfolio">
+        Portfolio
+      </router-link>
+      <router-link class="mr-4 md:mr-0" to="/skills"> Skills </router-link>
+      <router-link class="mr-4 md:mr-0" to="/about"> About </router-link>
       <router-link to="/contact"> Contact </router-link>
     </div>
-    <div id="links">
-      <span>links goes here</span>
+    <div id="links" class="flex justify mx-auto mt-4 mb-2">
+      <img src="../../assets/svgs/email.svg" alt="Email SVG" class="mr-4" />
+      <img
+        src="../../assets/svgs/github_circular.svg"
+        alt="Github SVG"
+        class="mr-4"
+      />
+      <img
+        src="../../assets/svgs/linkedin_circular.svg"
+        alt="LinkedIn SVG"
+        class="mr-4"
+      />
+      <img src="../../assets/svgs/instagram_circular.svg" alt="Instagram SVG" />
     </div>
   </div>
 </template>
